9196aabfc2697e6f10904d86b40b55f2c98bae5d
wenze_lv@aiagain.com <wenze_lv@aiagain.com>
2021-09-02 19:20:21 +0800

    初始化

---
+++ OcelotDemo/OcelotDemo/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
+using OcelotDemo.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OcelotDemo
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc((option) =>
+            {
+                option.EnableEndpointRouting = false;
+            });
+
+            services.SetSwaggerOption();//≈‰÷√swagger
+
+            services
+                .AddOcelot()
+                .AddConsul()
+                .AddPolly();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseSwaggerMiddleware();// π”√swagger
+
+            
+
+            app.UseMvc();
+            app.UseOcelot().Wait();
+        }
+    }
+}
---
+++ OcelotDemo/WebSite_B/Controller/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite_B.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("Get WebSite_B");
+        }
+
+        [HttpPut("Put")]
+        public async Task<string> Put()
+        {
+            return await Task.FromResult("Put WebSite_B");
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<string> Delete()
+        {
+            return await Task.FromResult("Delete WebSite_B");
+        }
+
+        [HttpOptions("Options")]
+        public async Task<string> Options()
+        {
+            return await Task.FromResult("Options WebSite_B");
+        }
+
+        [HttpGet("Health")]
+        public async Task<string> Health()
+        {
+            return await Task.FromResult("Ok");
+        }
+    }
+}
